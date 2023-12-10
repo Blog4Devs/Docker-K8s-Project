d1e59f4abe884ed56da6cfc1811c288fd387f119
‘ELMILYASS’ <ilyasselmazidi123@gmail.com>
2023-12-10 22:38:46 +0100

    finished

---
+++ spring-boot-data-jpa-mysql-master/src/main/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplication.java
@@ -0,0 +1,16 @@
+package com.bezkoder.spring.datajpa;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataJpaApplication.class, args);
+	}
+
+
+
+}
